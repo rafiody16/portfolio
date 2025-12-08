4fd5113494ee697c04da86b9f5ada0bef61dfebf
RAFI ODY PRASETYO <53549077+rafiody16@users.noreply.github.com>
2025-12-09 02:57:58 +0700

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Navbar from './components/Navbar.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Navbar from "./components/Navbar.jsx";
+import { HashRouter } from "react-router-dom"; 
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className='container mx-auto px-4'>
-      <Navbar />
-      <App />
-    </div>
-  </StrictMode>,
-)
+    <HashRouter> 
+      <div className="container mx-auto px-4">
+        <Navbar />
+        <App />
+      </div>
+    </HashRouter>
+  </StrictMode>
+);
